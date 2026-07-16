334e3f249361a6b753307cec3cd2ca895337ef32
박영진 <wsbg@naver.com>
2026-07-16 11:00:13 +0900

    CORS 주소 슬래시 변경

--- LocalHub/backend/main.py
+++ LocalHub/backend/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", 
                    "http://localhost:8000",
-                   "https://localhub-b04-pyj-ljm.netlify.app/"],
+                   "https://localhub-b04-pyj-ljm.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
